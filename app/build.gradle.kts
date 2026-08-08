0b33972eea75ebc0fd3649936f0cdd5fcadfe662
anirbansumon <anirbansumon89@gmail.com>
2026-08-08 19:11:08 +0600

    update files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,10 +90,6 @@ android {
 
 // Configure the Secrets Gradle Plugin to use .env and .env.example files
 // to match the convention used in Web projects.
-secrets {
-  propertiesFileName = ".env"
-  defaultPropertiesFileName = ".env.example"
-}
 
 // Some unused dependencies are commented out below instead of being removed.
 // This makes it easy to add them back in the future if needed.
