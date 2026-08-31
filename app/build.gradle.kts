97e80d9e65804ce959125e21eb0857c3b1c83046
anirbansumon <anirbansumon89@gmail.com>
2026-08-31 08:54:02 +0600

    update files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,17 @@ android {
     debug { signingConfig = signingConfigs.getByName("debugConfig") }
   }
 
+splits {
+    abi {
+      isEnable = true
+      reset()
+      include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+      isUniversalApk = true
+    }
+  }
+
+
+
   packaging {
     resources {
       excludes += setOf(
